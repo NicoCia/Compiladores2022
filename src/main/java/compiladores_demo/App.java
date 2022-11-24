eaf0b2dbd0381d9e6426b3ceb7a9ff471e30278e
NicoCia <nicocia7@gmail.com>
2022-11-24 19:04:39 -0300

    Inicializar

---
+++ src/main/java/compiladores_demo/App.java
@@ -0,0 +1,13 @@
+package compiladores_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
